acf14172e6326fbe9def52e453268a9b57ce4992
Steven Olufemi-OluKa <Steven.Olufemi-OluKanni@jet2.com>
2019-12-10 17:19:14 +0000

    Add project files.

---
+++ Pricing App/Checkout.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pricing_App
+{
+    public class Checkout
+    {
+        private PricingRules _pricingRules;
+
+        public Checkout(PricingRules pricingRules)
+        {
+            _pricingRules = pricingRules;
+        }
+
+        public int Total { get; set; }
+
+        public void Scan(string item)
+        {
+            // go through each special rule and check if it applies
+            // for e.g. is 3As present? return the rule
+            // does the item contain the letter A three times?
+            // if so, calculate cost, and remove the items from the list
+
+            var rule = _pricingRules.Items.Where(pricingRule => item.Count(x => x.ToString() == pricingRule.Name) == pricingRule.SpecialPrice.Quantity);
+            //Total += _pricingRules.Items.First(x => x.Name == item).UnitPrice;
+        }
+    }
+
+    public class PricingRules
+    {
+        public List<PricingRule> Items { get; set; }
+    }
+
+
+    public class PricingRule
+    {
+        public string Name { get; set; }
+        public int UnitPrice { get; set; }
+        public SpecialPrice SpecialPrice { get; set; }
+    }
+
+    public class SpecialPrice
+    {
+        public int Quantity { get; set; }
+
+        public int Price { get; set; }
+    }
+}
---
+++ Pricing App/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Pricing_App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var pricingRules = new PricingRules
+            {
+                Items = new List<PricingRule>
+                {
+                    new PricingRule
+                    {
+                        Name = "A",
+                        SpecialPrice = new SpecialPrice
+                        {
+                            Price = 130,
+                            Quantity = 3
+                        },
+                        UnitPrice = 50
+                    },
+                    new PricingRule
+                    {
+                        Name = "B",
+                        SpecialPrice = new SpecialPrice
+                        {
+                            Price = 45,
+                            Quantity = 2
+                        },
+                        UnitPrice = 30
+                    },
+                    new PricingRule
+                    {
+                        Name = "C",
+                        UnitPrice = 20
+                    }
+                }
+               
+            };
+
+            var checkout = new Checkout(pricingRules);
+
+            var itemsList = "AAABB";
+            checkout.Scan(itemsList);
+
+            //foreach (var item in itemsList)
+            //{
+            //    checkout.Scan(item.ToString());
+            //}
+
+            var price = checkout.Total;
+        }
+    }
+}
